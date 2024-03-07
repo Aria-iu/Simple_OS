55a6011d97be38d695bb8331d58e88b5f9694492
Aria-iu <2357648739@qq.com>
2024-03-07 10:36:42 +0800

    add apps

---
+++ apps/target/riscv64gc-unknown-none-elf/release/deps/00hello_world-25c97b414b2fc73e.d
@@ -0,0 +1,5 @@
+/home/zyc/rust_projects/Simple_Core/apps/target/riscv64gc-unknown-none-elf/release/deps/00hello_world-25c97b414b2fc73e: src/bin/00hello_world.rs
+
+/home/zyc/rust_projects/Simple_Core/apps/target/riscv64gc-unknown-none-elf/release/deps/00hello_world-25c97b414b2fc73e.d: src/bin/00hello_world.rs
+
+src/bin/00hello_world.rs:
---
+++ apps/target/riscv64gc-unknown-none-elf/release/deps/02power-9d1b75a7ba9c0bee.d
@@ -0,0 +1,5 @@
+/home/zyc/rust_projects/Simple_Core/apps/target/riscv64gc-unknown-none-elf/release/deps/02power-9d1b75a7ba9c0bee: src/bin/02power.rs
+
+/home/zyc/rust_projects/Simple_Core/apps/target/riscv64gc-unknown-none-elf/release/deps/02power-9d1b75a7ba9c0bee.d: src/bin/02power.rs
+
+src/bin/02power.rs:
